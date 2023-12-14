2610ab48dd9ceaf5411956194d13f7676815b426
김성은 <kse012050@naver.com>
2023-12-15 03:57:15 +0900

    메인 - 마케팅 서비스 애니메이션 완료

--- js/style.js
+++ js/style.js
@@ -25,8 +25,23 @@ function commonStyle(){
 
     // 풀 세로 사이즈
     $('body').css('--fullHeight', window.innerHeight + 'px');
+    $('body').css('--fullWidth', window.innerWidth + 'px');
+    let scrollWidth;
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        scrollWidth = '0'
+    } else {
+        scrollWidth = '17'
+    }
+    $('body').css('--scrollWidth', scrollWidth + 'px');
     $(window).resize(function(){
         $('body').css('--fullHeight', window.innerHeight + 'px');
+        $('body').css('--fullWidth', window.innerWidth + 'px');
+        if (/Mobi|Android/i.test(navigator.userAgent)) {
+            scrollWidth = '0'
+        } else {
+            scrollWidth = '17'
+        }
+        $('body').css('--scrollWidth', scrollWidth + 'px');
     })
 }
 
@@ -43,7 +58,6 @@ function styleIdx(){
         $('[data-scrollPosition]').each(function(){
             $(this)[0].getBoundingClientRect().y < 0 ? $(this).addClass('active') : $(this).removeClass('active');
         })
-
     })
 }
 
@@ -54,4 +68,21 @@ function mainPage(){
             el: ".swiper-pagination",
         },
     });
+
+    $(window).scroll(function(){
+        if($('.serviceScroll')[0].getBoundingClientRect().y <= 0 && $(window).height() - $('.serviceScroll').height() < $('.serviceScroll')[0].getBoundingClientRect().y){
+            $('.serviceScroll ul li').each(function(i){
+                if(($('.serviceScroll').height() - $(window).height()) / $('.serviceScroll ul li').length * i < $('.serviceScroll')[0].getBoundingClientRect().y * -1){
+                    $(this).addClass('active')
+                    $('.serviceScroll > div ol li').eq(i).addClass('active');
+                }else{
+                    $(this).removeClass('active')
+                    $('.serviceScroll > div ol li').eq(i).removeClass('active');
+                }
+            })
+        }
+
+        //     && $('.serviceTest ul').stop().css('top', test)
+            // $('.serviceTest div').css('top', Math.abs($('.serviceTest')[0].getBoundingClientRect().y))
+    })
 }
